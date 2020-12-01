a9ec552954c58bca6249e7ae1e2c7731fcd5b1a8
Alphabeater <19984045+Alphabeater@users.noreply.github.com>
2020-12-01 11:15:51 -0300

    Stage 1/6: Numbers only

--- Main.kt
+++ Main.kt
@@ -1,5 +1,14 @@
 package sorting
+import java.util.*
+
+val s = Scanner(System.`in`)
 
 fun main() {
-    // write your code here
+    val numbers = mutableListOf<Int>()
+    do {
+        numbers += s.nextInt()
+    } while(s.hasNext())
+    val maxNumber = numbers.max()
+    println("Total numbers: ${numbers.size}.\n" +
+            "The greatest number: $maxNumber (${numbers.count { it == maxNumber }} time(s)).")
 }
